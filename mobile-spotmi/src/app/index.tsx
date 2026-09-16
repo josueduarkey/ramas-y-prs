7df38715ab31910e40a086220a33f5e2558279a7
Rafael <rafasanguti69@gmail.com>
2026-09-16 08:49:21 -0600

    style: lowercase for text

--- mobile-spotmi/src/app/index.tsx
+++ mobile-spotmi/src/app/index.tsx
@@ -86,7 +86,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   code: {
-    textTransform: 'uppercase',
+    textTransform: 'lowercase',
   },
   stepContainer: {
     gap: Spacing.three,

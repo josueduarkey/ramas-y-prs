bc0ae22b7ff1bed28a4885dd13cfd0ea8ae586fe
Rafael <rafasanguti69@gmail.com>
2026-09-16 08:48:12 -0600

    style: new order

--- mobile-spotmi/src/app/index.tsx
+++ mobile-spotmi/src/app/index.tsx
@@ -64,7 +64,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     flexDirection: 'row',
   },
   safeArea: {
